c73c3723a5488a3ef8e1d7f6f2b839289cc07003
Joubarbe <joubarbe@ymail.com>
2017-03-13 12:01:14 +0100

    properly isolated "lab" code

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,22 +5,13 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
 
     public function indexAction()
     {
-        $url = $this
-            ->generateUrl("oc_platform_view", ["id" => 2], UrlGeneratorInterface::ABSOLUTE_URL);
-        // Controller shortcut for: $this->get("router")->generate(...);
-
-        $content = $this
-            ->get("templating")
-            ->render("OCPlatformBundle:Advert:index.html.twig", ["name" => "winzou", "url" => $url, "advert_id" => 3]);
-
-        return new Response($content);
+        return new Response("index");
     }
 
     public function viewAction($id)
--- src/OC/PlatformBundle/Controller/LabController.php
+++ src/OC/PlatformBundle/Controller/LabController.php
@@ -6,11 +6,26 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LabController extends Controller
 {
     public function viewSlugAction($slug, $year, $_format)
     {
-        return new Response("slug/year/format = " . $slug . $year . $_format);
+        $url = $this
+            ->generateUrl("oc_platform_view", ["id" => 2], UrlGeneratorInterface::ABSOLUTE_URL);
+        // Controller shortcut for: $this->get("router")->generate(...);
+
+        $content = $this
+            ->get("templating")
+            ->render("OCPlatformBundle:Lab:index.html.twig", [
+                "name" => "winzou",
+                "url" => $url, "advert_id" => 3,
+                "slug" => $slug,
+                "year" => $year,
+                "format" => $_format,
+            ]);
+
+        return new Response($content);
     }
 }
